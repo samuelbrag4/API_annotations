554f2fbccac8479c92151341050b0d4601357175
Samuel Braga <samuelbraga.tds2.senai@gmail.com>
2025-03-27 13:39:33 -0300

    feat: update routes to use NotaController for note management

--- src/routes/notaRoutes.js
+++ src/routes/notaRoutes.js
@@ -1,8 +1,8 @@
 import express from "express";
-import tarefaController from "../controllers/notaController.js";
+import notaController from "../controllers/notaController.js";
 const router = express.Router();
-router.get("/", tarefaController.getAll);
-router.post("/", tarefaController.create);
-router.put("/:id", tarefaController.update);
-router.delete("/:id", tarefaController.delete);
+router.get("/", notaController.getAll);
+router.post("/", notaController.create);
+router.put("/:id", notaController.update);
+router.delete("/:id", notaController.delete);
 export default router;
